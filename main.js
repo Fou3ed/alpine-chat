43cafc13395ab6f2ce8f8ce8c2b2e18837c374ab
fou3ed <hamm.oudafoued@outlook.com>
2023-02-15 18:24:01 +0100

    crying

--- main.js
+++ main.js
@@ -9,15 +9,15 @@ const messageInput=document.getElementById("message-input")
 
 
 //connect to the socket when press the login button and redirect you to the index.html file 
+
 window.connected = async () => {
-  foued.connect(data.onConnect);
+  foued.connect(data.onConnect)
   return await foued.onConnected().then((newData) => {
     if (newData) {
       window.location.href = "./index.html"
     } else {
       alert("error")
     }
-
   });
 };
 
@@ -31,32 +31,7 @@ window.createConversation = async () => {
       alert('error creating the conversation ')
     }
   })
-
 }
-
-// window.sendMsg=async()=>{
-//   const info={
-//     app: "638dc76312488c6bf67e8fc0",
-//     user: "6390b2efdfb49a27e7e3c0b9",
-//     action: "message.create",
-//     metaData: {
-//         type: "MSG",
-//         conversation_id: "63971dd761f3ef13725a96d2",
-//         user:"6390b2efdfb49a27e7e3c0b9",
-//         message: messageInput.value,
-//         data:"non other data",
-//         origin:"web",
-//     },
-//   }
-
-//     foued.createMessage(info)
-//     document.querySelector("#message").innerText=info.metaData.message
-//     document.querySelector("#date_msg").innerText=fullDate
-//     messageInput.value = "";
-//     createMsg(info.metaData.message)
- 
-    
-// }
 
 
 document.querySelector('#send-message').addEventListener('click', () => {
@@ -75,22 +50,25 @@ document.querySelector('#send-message').addEventListener('click', () => {
   }
 
     foued.createMessage(info)
-    document.querySelector("#message").innerText=info.metaData.message
-    document.querySelector("#date_msg").innerText=fullDate
     messageInput.value = "";
     createMsg(info.metaData.message)
 })
 
-foued.onMessageDelivered().then(res => {
+
+window.createMsg=function(msg,direction="in"){
+
+
+  foued.onMessageDelivered().then(res => {
   let direction = "out";
   console.log("res", res);
   createMsg(res.content, direction);
 })
 
-window.createMsg=function(msg,direction="in"){
+
 messagesContainer.insertAdjacentHTML("beforeend", `<div class="flex items-start ${direction == 'in' ? 'justify-end' : ''} space-x-2.5 sm:space-x-5">
 <div class="flex flex-col items-end space-y-3.5">
   <div class="flex flex-row">
+
     <div x-data="usePopper({placement:'bottom-end',offset:4})" @click.outside="isShowPopper &amp;&amp; (isShowPopper = false)" class="inline-flex mt-2">
       <button x-ref="popperRef" @click="isShowPopper = !isShowPopper" class="btn h-8 w-8 rounded-full p-0 hover:bg-slate-300/20 focus:bg-slate-300/20 active:bg-slate-300/25 dark:hover:bg-navy-300/20 dark:focus:bg-navy-300/20 dark:active:bg-navy-300/25">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
@@ -98,6 +76,7 @@ messagesContainer.insertAdjacentHTML("beforeend", `<div class="flex items-start 
           </path>
         </svg>
       </button>
+      
       <div x-ref="popperRoot" class="popper-root" :class="isShowPopper &amp;&amp; 'show'" style="position: fixed; inset: 0px 0px auto auto; margin: 0px; transform: translate(-594px, 231px);" data-popper-placement="bottom-end">
         <div class="popper-box rounded-md border border-slate-150 bg-white py-1.5 font-inter dark:border-navy-500 dark:bg-navy-700">
           <ul>
@@ -131,7 +110,9 @@ messagesContainer.insertAdjacentHTML("beforeend", `<div class="flex items-start 
               ${fullDate}      
       </p>
     </div>
+  
   </div>
+  
   <div class="flex flex-row">
       </div>
     </div>
